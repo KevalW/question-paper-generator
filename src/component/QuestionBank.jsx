6358548498b2ef3ae48deaa44783ea634fe0e602
keval <keval.waghate@gmail.com>
2023-08-14 11:44:09 -0700

    added marks column

--- src/component/QuestionBank.jsx
+++ src/component/QuestionBank.jsx
@@ -5,34 +5,37 @@ import '../styling/QuestionBank.css'
 function QuestionBank() {
     const [boxs, setBoxs] = useState([]);
 
-    const addvalue = () => {
-        const tempx = [...boxs, []]
-        setBoxs(tempx);
+    const addValue = () => {
+        const tempArr = [...boxs, { value1: '', value2: '' }];
+        setBoxs(tempArr);
     }
-    const handleChange =(onChangeValue,i)=>{
-        const inputData = [...boxs]
-        inputData[i] = onChangeValue.target.value;
-        setBoxs(inputData)
+
+    const handleChange = (event, i, valueKey) => {
+        const updatedData = [...boxs];
+        updatedData[i][valueKey] = event.target.value;
+        setBoxs(updatedData);
     }
     console.log(boxs)
-    const handledelete = (i) => {
-        const deleteValue = [...boxs]
-        deleteValue.splice(i,1)
-        setBoxs(deleteValue)
+
+    const handleDelete = (i) => {
+        const updatedArr = [...boxs];
+        updatedArr.splice(i, 1);
+        setBoxs(updatedArr);
     }
 
 
     return ( 
-        <div className="question-bank"> 
-            {boxs.map((data,i) =>{
-                return(
-                    <div className="question-container">
-                        <input  className = 'question-box' type="text" placeholder="question" value = {data} onChange={e=>handleChange(e,i)}/>
-                        <button className="delete-button" onClick={() => handledelete(i)}>x</button>
+        <div className="question-bank">
+            {boxs.map((data, i) => {
+                return (
+                    <div className="question-container" key={i}>
+                        <input className='question-box' type="text" placeholder="Value 1" value={data.value1} onChange={e => handleChange(e, i, 'value1')} />
+                        <input className='question-box' type="text" placeholder="Value 2" value={data.value2} onChange={e => handleChange(e, i, 'value2')} />
+                        <button className="delete-button" onClick={() => handleDelete(i)}>x</button>
                     </div>
                 )
             })}
-            <button className="add-value" onClick={()=> addvalue()}> Click Here</button>
+            <button className="add-value" onClick={() => addValue()}>Click Here</button>
         </div>
     );
 }
