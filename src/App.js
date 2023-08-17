73615f9295897d64a79ce010695aa5b55827219e
keval <keval.waghate@gmail.com>
2023-08-17 07:09:14 -0700

    changing teh directory

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css'
+
+import './index.css';
 import QuestionBank from './component/QuestionBank';
 
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import 'firebase/firestore';
+import 'firebase/storage';
+
+
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBMI8NB7eyXQPyLHE71uYORXTxwFE8hZBE",
+    authDomain: "question-paper-generator-8e829.firebaseapp.com",
+    projectId: "question-paper-generator-8e829",
+    storageBucket: "question-paper-generator-8e829.appspot.com",
+    messagingSenderId: "1029704276927",
+    appId: "1:1029704276927:web:9d97551809c089c1629f0a",
+    measurementId: "G-ZYG2NRQEVV"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  
+
+
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<QuestionBank/>}></Route>
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<QuestionBank />}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/component/QuestionBank.jsx
+++ src/component/QuestionBank.jsx
@@ -1,66 +1,103 @@
-import React, { useState } from "react";
-import "../styling/QuestionBank.css";
+import React, { useState } from 'react';
+import '../styling/QuestionBank.css';
+
+import firebase from 'firebase/compat/app'; // this is changes from the version keep it in mind
+import 'firebase/firestore';
+import 'firebase/storage';
+
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { firebaseApp } from '@firebase/app'; // Adjust the path if needed
 
 function QuestionBank() {
     const [boxs, setBoxs] = useState([]);
 
+
+    // const db = firebase.firestore();
+
     const addValue = () => {
-        const tempArr = [...boxs, { marks: "", question_value: "", image: null }];
+        const tempArr = [...boxs, { marks: '', question_value: '', image: null }];
         setBoxs(tempArr);
-    };
+    }
 
     const handleChange = (event, i, valueKey) => {
         const updatedData = [...boxs];
         updatedData[i][valueKey] = event.target.value;
         setBoxs(updatedData);
-    };
+    }
 
-    const handleImageChange = (event, i) => {
+    const handleImageChange = async (event, i) => {
         const updatedData = [...boxs];
-        updatedData[i].image = event.target.files[0];
+        const file = event.target.files[0];
+
+        const storage = getStorage(firebaseApp); // Use your Firebase app instance here
+        const storageRef = ref(storage, 'images/' + file.name);
+        await uploadBytes(storageRef, file);
+
+        const imageUrl = await getDownloadURL(storageRef);
+
+        updatedData[i].image = imageUrl;
         setBoxs(updatedData);
     };
+
+
 
     const handleDelete = (i) => {
         const updatedArr = [...boxs];
         updatedArr.splice(i, 1);
         setBoxs(updatedArr);
     };
-    console.log(boxs)
+
+    const handleFormSubmit = async () => {
+        try {
+            const collectionRef = firebase.firestore().collection('/questionBank/hM426ti9aNIdotTvvpvR '); // Replace with your collection name
+
+            for (const item of boxs) {
+                const data = {
+                    marks: item.marks,
+                    question_value: item.question_value,
+                    image: item.image
+                };
+
+                await collectionRef.add(data);
+            }
+
+            console.log('Data uploaded successfully');
+        } catch (error) {
+            console.error('Error uploading data:', error);
+        }
+    }
+
     return (
         <div className="question-bank">
             {boxs.map((data, i) => {
                 return (
                     <div className="question-container" key={i}>
                         <input
-                            className="question-box-marks"
+                            className='question-box-marks'
                             type="text"
                             placeholder="Marks"
                             value={data.marks}
-                            onChange={(e) => handleChange(e, i, "marks")}
+                            onChange={e => handleChange(e, i, 'marks')}
                         />
                         <input
-                            className="question-box"
+                            className='question-box'
                             type="text"
                             placeholder="Question"
                             value={data.question_value}
-                            onChange={(e) => handleChange(e, i, "question_value")}
+                            onChange={e => handleChange(e, i, 'question_value')}
                         />
                         <input
-                            className="question-box image-upload"
+                            className='question-box image-upload'
                             type="file"
                             accept="image/*"
-                            onChange={(e) => handleImageChange(e, i)}
+                            onChange={e => handleImageChange(e, i)}
                         />
-                        <button className="delete-button" onClick={() => handleDelete(i)}>
-                            x
-                        </button>
+                        <button className="delete-button" onClick={() => handleDelete(i)}>x</button>
                     </div>
-                );
+                )
             })}
-            <button className="add-value" onClick={() => addValue()}>
-                Click Here
-            </button>
+            <button className="add-value" onClick={() => addValue()}>Click Here</button>
+            <button className="submit-button" onClick={handleFormSubmit}>Submit</button>
         </div>
     );
 }
