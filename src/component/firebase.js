3261883f8d30b0ae5e90365fa397a5c175085372
keval <keval.waghate@gmail.com>
2023-08-22 00:50:06 +0530

    lots of changes in user interface and data collection

--- src/component/firebase.js
+++ src/component/firebase.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import styles from '../styling/QuestionBank.css'; // Correct the path based on your file structure
-
+import styles from '../styling/QuestionBank.css';
 
 function QuestionBank() {
     const [data, setData] = useState([]);
@@ -20,37 +19,12 @@ function QuestionBank() {
     };
 
     return (
-        <div>
+        <div className={styles.container}>
             <table className={`table ${styles.customTable}`}>
-                <thead>
-                    <tr>
-                        <th>Module</th>
-                        <th>Marks</th>
-                        <th>Question</th>
-                        <th>Image</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((item, index) => (
-                        <tr key={index}>
-                            <td>{item.module}</td>
-                            <td>{item.marks}</td>
-                            <td>{item.question}</td>
-                            <td>
-                                {item.image && (
-                                    <img
-                                        src={URL.createObjectURL(item.image)}
-                                        alt={`User ${item.module}`}
-                                        className={styles.tableImage}
-                                    />
-                                )}
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
+                {/* ... */}
             </table>
             <form onSubmit={handleSubmit} className={styles.formContainer}>
-                <div className="row">
+                <div className={`row ${styles.inputRow}`}>
                     <div className="col-md-3">
                         <input
                             type="text"
@@ -59,8 +33,6 @@ function QuestionBank() {
                             value={module}
                             onChange={(e) => setModule(e.target.value)}
                         />
-                    </div>
-                    <div className="col-md-2">
                         <input
                             type="number"
                             className="form-control"
@@ -68,8 +40,6 @@ function QuestionBank() {
                             value={marks}
                             onChange={(e) => setMarks(e.target.value)}
                         />
-                    </div>
-                    <div className="col-md-5">
                         <input
                             type="text"
                             className="form-control"
@@ -77,22 +47,11 @@ function QuestionBank() {
                             value={question}
                             onChange={(e) => setQuestion(e.target.value)}
                         />
-                    </div>
-                    <div className="col-md-2">
-                        <input
-                            type="file"
-                            accept="image/*"
-                            className="form-control-file"
-                            onChange={(e) => setImage(e.target.files[0])}
-                        />
-                    </div>
-                    <div className="col-md-12 mt-2">
                         <button type="submit" className="btn btn-primary btn-block">
                             Add
                         </button>
                     </div>
                 </div>
-
             </form>
         </div>
     );
